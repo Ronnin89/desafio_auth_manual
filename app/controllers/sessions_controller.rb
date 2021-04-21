e86da09b397025ab468de920653c1893ac0d7b57
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-04-20 22:31:19 -0400

    fix session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,24 @@
 class SessionsController < ApplicationController
-    def new
-    end
   
-    def create
-      user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
-        session[:user_id] = user.id
-        redirect_to root_url, :notice => "Autenticación exitosa"
-      else
-        redirect_to root_url, :alert => "Las credenciales son incorrectas"
-      end
-    end
+  def new
+    @user = User.new
+  end
   
-    def destroy
-      session.delete(:user_id)
-  
-      redirect_to root_url, :notice => "Hasta pronto"
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    pass = BCrypt::Password.new(@user.password_digest)
+
+    if @user.present? && pass == params[:user][:password]
+        session[:user_id] = @user.id
+        redirect_to root_path, notice: 'has iniciado session'
+    else
+        redirect_to new_user_session_path, notice: 'usuario y/o clave invalido'
     end
   end
+  
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'has cerrado la sesion'
+  end 
+
+end
